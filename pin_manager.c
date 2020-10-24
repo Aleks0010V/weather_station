51d7fbb1a9ad9da1b251f737b0d97d7dbd49b36f
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2020-10-24 13:31:06 +0300

    pin config changes

--- pin_manager.c
+++ pin_manager.c
@@ -25,9 +25,10 @@ void pin_Initiasize(void) {
     // 0 - output, 1 - input
     TRISA = 0b01000000;
     TRISB = 0;
-    // MSSP1 as I2C pins should be inputs, MSSP2 SPI RC4 - CLK input
+    // RC2 - MSSP1 I2C SDA.
+    // RC3 - MSSP1 I2C SCL.
     TRISC = 0b00001100;
-    // RD4 - MSSP2 SPI input, RD6 - output, RD7 - clock input, 
-    // RD6 - clock output
-    TRISD = 0b10010000;
+    // RD2 - MSSP2 SPI input, RD3 - output
+    // RD1 - clock output
+    TRISD = 0b00000100;
 }
