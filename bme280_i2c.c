9c33784c34bf480f9fc73c851160aa29b10f60ad
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2020-10-24 22:09:32 +0300

    compensate functions init

--- bme280_i2c.c
+++ bme280_i2c.c
@@ -8,6 +8,7 @@
 
 #include <xc.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include "system.h"
 #include "I2C.h"
 #include "bme280_i2c.h"
@@ -23,6 +24,19 @@ static void reset(void);
 static void update_data(void);
 
 static uint8_t current_addr = 0;
+static int32_t t_fine;  // t_fine carries fine temperature as global value
+
+// data from calibration registers
+static uint16_t dig_T1;
+static uint16_t dig_P1;
+static int16_t dig_t[2];
+static int16_t dig_p[8];
+static uint8_t dig_H1;
+static int16_t dig_H2;
+static uint8_t dig_H3;
+static int16_t dig_H4;
+static int16_t dig_H5;
+static int8_t dig_H6;
 
 bme_data current_data;
 
@@ -74,6 +88,52 @@ bool bme280_exists(void) {
         return true;
     } else
         return false;
+}
+
+int32_t compensate_temperature(int32_t adc_temp) {
+    // Returns temperature in DegC, resolution is 0.01 DegC. Output value of ?5123?equals 51.23 DegC.
+    int32_t var1, var2, T;
+    var1 = (( ( (adc_temp >> 3) - ( (int32_t)dig_T1 << 1) )) * ((int32_t)dig_t[0])) >> 11;
+    var2  = (((((adc_temp>>4) - ((int32_t)dig_T1)) * ((adc_temp>>4) - ((int32_t)dig_T1))) >> 12) * ((int32_t)dig_t[1])) >> 14;
+    t_fine = var1 + var2;
+    T  = (t_fine * 5 + 128) >> 8;
+    return T;
+}
+
+uint32_t compensate_pressure(int32_t adc_pres) {
+    // Returns pressure in Pa as unsigned 32 bit integer in Q24.8 format (24 integer bits and 8 fractional bits).
+    // Output value of ?24674867?represents 24674867/256 = 96386.2 Pa = 963.862 hPa
+    int64_t var1, var2, p;
+    var1 = ((int64_t)t_fine) - 128000;
+    var2 = var1 * var1 * (int64_t)dig_p[4];
+    var2 = var2 + ((var1*(int64_t)dig_p[3]) << 17);
+    var2 = var2 + (((int64_t)dig_p[2]) << 35);
+    var1 = ((var1 * var1 * (int64_t)dig_p[1]) >> 8) + ((var1 * (int64_t)dig_p[0]) << 12);
+    var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)dig_P1) >> 33;
+    if (var1 == 0) {
+        return 0;
+    }
+    p = 1048576 - adc_pres;
+    p = (((p << 31) - var2) * 3125) / var1;
+    var1 = (((int64_t)dig_p[7]) * (p >> 13) * (p >> 13)) >> 25;
+    var2 = (((int64_t)dig_p[6]) * p) >> 19;
+    p = ((p + var1 + var2) >> 8) + (((int64_t)dig_p[5]) << 4);
+    return (uint32_t)p;
+}
+
+uint32_t compensate_humidity(int32_t adc_hum) {
+    // Returns humidity in %RHas unsigned 32 bit integer in Q22.10format (22integer and 10fractional bits).
+    // Output value of ?47445?represents 47445/1024= 46.333%RH
+    int32_t v_x1_u32r;
+    v_x1_u32r = (t_fine - ((int32_t)76800));
+    v_x1_u32r = (((((adc_hum << 14) - (((int32_t)dig_H4) << 20) - (((int32_t)dig_H5) * v_x1_u32r)) +
+            ((int32_t)16384)) >> 15) * (((((((v_x1_u32r * ((int32_t)dig_H6)) >> 10) * (((v_x1_u32r *
+            ((int32_t)dig_H3)) >> 11) + ((int32_t)32768))) >> 10) + ((int32_t)2097152)) *
+            ((int32_t)dig_H2) + 8192) >> 14));
+    v_x1_u32r = (v_x1_u32r - (((((v_x1_u32r >> 15) * (v_x1_u32r >> 15)) >>7) * ((int32_t)dig_H1)) >> 4));
+    v_x1_u32r = (v_x1_u32r < 0 ? 0 : v_x1_u32r);
+    v_x1_u32r = (v_x1_u32r > 419430400 ? 419430400 : v_x1_u32r);
+    return(uint32_t)(v_x1_u32r >> 12);
 }
 
 static bool check_id(uint8_t addr) {
--- bme280_i2c.h
+++ bme280_i2c.h
@@ -19,9 +19,16 @@ extern "C" {
         int32_t temperature;
         int32_t humidity;
     } bme_data;
+    typedef long long signed int int64_t;
     
     bool bme280_exists(void);
     void bme280_Initialize(void);
+    
+    // these 3 functions take 33% of PIC16F18875 memory - 4730.88 bytes.
+    // compensate_pressure - most of it.
+    int32_t compensate_temperature(int32_t adc_temp);
+    uint32_t compensate_pressure(int32_t adc_pres);
+    uint32_t compensate_humidity(int32_t adc_hum);
 
 
 #ifdef	__cplusplus
