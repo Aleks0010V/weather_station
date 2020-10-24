3ee845fcc2304f93553c43596d8d35e636158b1c
Aleks Anatoliyovich <44201618+Aleks0010V@users.noreply.github.com>
2020-10-24 16:03:58 +0300

    dummy bytes for card init

--- SD.c
+++ SD.c
@@ -13,6 +13,8 @@
 static const SD_command reset = {CMD0, 0, 0, 0, 0, 149};
 
 void SD_initialize(void) {
+    uint8_t dummy[] = {255, 255, 255, 255, 255, 255, 255, 255, 255, 255};
+    spi_write(dummy, 10);
     spi_write(&reset, 6);
     uint8_t result = 0;
     spi_read(&result, 1, 255);
